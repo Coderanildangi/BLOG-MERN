08c39cee6fbf285a810dd02dcd6e421c72306506
Coderanildangi <anil.dangi@cctech.co.in>
2024-01-24 15:36:57 +0530

    Added indiviual Page files.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignIn from '../Pages/SignIn'
+import SignUp from '../Pages/SignUp'
+import Project from '../Pages/Project'
+import Dashboard from '../Pages/Dashboard'
+import About from '../Pages/About'
+import Home from '../Pages/Home'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="/about" element = {<About />} />
+        <Route path='/sign-in' element = {<SignIn />} />
+        <Route path='/sign-up' element = {<SignUp />} />
+        <Route path='/dashboard' element = {<Dashboard />} />
+        <Route path='/project' element = {<Project />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
